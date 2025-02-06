08c4b261831fa6568631cf75802208a2d8a25a20
M1NGJU <s2303@e-mirim.hs.kr>
2025-02-06 18:27:23 +0900

    영상 스타일 변경

--- youtube.php
+++ youtube.php
@@ -2,7 +2,7 @@
 include 'config.php'; // API 키 저장된 파일 포함
 
 $channelId = "UCiZqWVAeChfqlom5ZPR3ZJA"; // 유튜브 채널 ID (변경 가능)
-$maxResults = 5; // 가져올 최신 동영상 개수
+$maxResults = 8; // 가져올 최신 동영상 개수
 
 // API 요청 URL
 $url = "https://www.googleapis.com/youtube/v3/search?order=date&part=snippet&type=video&channelId={$channelId}&maxResults={$maxResults}&key={$apiKey}";
@@ -14,24 +14,14 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 $response = curl_exec($ch);
 curl_close($ch);
 
-// JSON 데이터 파싱
 $data = json_decode($response, true);
 
-// 동영상 출력
 if (!empty($data['items'])) {
     foreach ($data['items'] as $item) {
         $videoId = $item['id']['videoId'];
-        $title = $item['snippet']['title'];
-        $thumbnail = $item['snippet']['thumbnails']['medium']['url'];
-        $videoUrl = "https://www.youtube.com/watch?v=" . $videoId;
 
-        echo '<div class="col-md-4 col-sm-6 col-12">';
-        echo '<div class="card mb-3">';
-        echo "<a href='{$videoUrl}' target='_blank'>";
-        echo "<img src='{$thumbnail}' class='card-img-top' alt='{$title}'>";
-        echo "</a>";
-        echo "<div class='card-body'><h6 class='card-title'>{$title}</h6></div>";
-        echo "</div>";
+        echo '<div class="youtube-video">';
+        echo "<iframe src='https://www.youtube.com/embed/{$videoId}' frameborder='0' allowfullscreen></iframe>";
         echo '</div>';
     }
 } else {
